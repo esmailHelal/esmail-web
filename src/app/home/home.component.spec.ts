b9fc66a5ccd4764ca4aff1dc1cc19ec29ce640f2
esmailll <32510704+esmailll@users.noreply.github.com>
2019-05-27 18:37:35 +0200

    first commit

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EhabComponent } from './ehab.component';
+import { HomeComponent } from './home.component';
 
-describe('EhabComponent', () => {
-  let component: EhabComponent;
-  let fixture: ComponentFixture<EhabComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EhabComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(EhabComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
